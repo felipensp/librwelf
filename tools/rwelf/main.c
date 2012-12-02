eec8e595dfa883f7fd782a09fd1548357cb6911a
Felipe Pena <felipensp@gmail.com>
2012-12-02 17:32:31 -0200

    - Update

--- tools/rwelf/main.c
+++ tools/rwelf/main.c
@@ -4,17 +4,18 @@
 int main(int argc, char **argv) {
 	rwelf *elf = rwelf_open("./librwelf.so");
 	
-	if (elf) {
-		printf("Class:    %s\n",  rwelf_class(elf));
-		printf("Data:     %s\n",  rwelf_data(elf));
-		printf("Version:  %d\n",  rwelf_version(elf));
-		printf("Type:     %s\n",  rwelf_type(elf));
-		printf("Sections: %d\n", rwelf_num_sections(elf));
-		printf("PHeaders: %d\n", rwelf_num_pheaders(elf));
-		printf("Entry:    %p\n", rwelf_entry(elf));
-		
-		rwelf_close(elf);
+	if (!elf) {
+		exit(1);
 	}
+	printf("Class:    %s\n",  rwelf_class(elf));
+	printf("Data:     %s\n",  rwelf_data(elf));
+	printf("Version:  %d\n",  rwelf_version(elf));
+	printf("Type:     %s\n",  rwelf_type(elf));
+	printf("Sections: %d\n", rwelf_num_sections(elf));
+	printf("PHeaders: %d\n", rwelf_num_pheaders(elf));
+	printf("Entry:    %p\n", rwelf_entry(elf));
+		
+	rwelf_close(elf);
 
 	return 0;
 }
